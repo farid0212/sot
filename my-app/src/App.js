0d3f5ad86398e28b38c48e97ed7c10a7b226dbc2
Farid <sul.far@bk.ru>
2023-05-11 10:32:38 +0300

    start

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img
+          className="logo"
+          src="hhttps://e7.pngegg.com/pngimages/615/494/png-clipart-logo-design-text-symbol.png"
+        ></img>
+      </header>
+      <nav className="nav">
+        <a>
+          <div>Profile</div>
+        </a>
+      </nav>
+      <div className="main"></div>
+    </div>
+  );
+}
+
+export default App;
